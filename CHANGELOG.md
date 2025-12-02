941d3f29d1335bb39ecf081bb3b53d6976254b69
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-02 16:19:59 +0900

    1.2.1

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# CHANGELOG
-
-## v0.1.0 (2025-12-01)
-- start reading
-- initializing repository
--- chap1/p4.scm
+++ chap1/p4.scm
@@ -2,5 +2,5 @@
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
-(print (a-plus-abs-b 1 2))
-(print (a-plus-abs-b 1 -2))
+; (print (a-plus-abs-b 1 2))
+; (print (a-plus-abs-b 1 -2))
